f814b1a15e06ad1158e7670708b35ca539bf8c71
brencon <brendan.conrad@gmail.com>
2025-04-14 12:07:39 -0700

    Initial commit for image-crop-engine

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  "env": {
+    "node": true,
+    "commonjs": true,
+    "es2021": true,
+    "jest": true,
+    "browser": true
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "ecmaVersion": 2022,
+    "sourceType": "module"
+  },
+  "rules": {
+    "indent": ["off"],
+    "linebreak-style": ["error", "unix"],
+    "quotes": ["error", "single"],
+    "semi": ["error", "always"],
+    "no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
+    "no-console": "warn",
+    "no-undef": "error",
+    "prefer-const": "error"
+  },
+  "globals": {
+    "window": "readonly",
+    "document": "readonly",
+    "requestAnimationFrame": "readonly",
+    "cancelAnimationFrame": "readonly"
+  }
+}; 
